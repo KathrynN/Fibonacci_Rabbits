187dd64ee16abb7ff28ad2fc1b217c085aa23f17
KathrynN <kathrynnewbould@yahoo.com>
2013-11-12 10:21:35 +0000

    BreedingTimeUpdate
    added some comments

--- BreedingTime.java
+++ BreedingTime.java
@@ -12,7 +12,7 @@ public class BreedingTime {
 		// Method to monitor the breeding / dying behaviour of rabbits.
 		for(int t = 0; t<time; t++){
 			if(rab.size() ==0){
-				break;
+				break;  // stop if all rabbits are dead
 			}
 			rab = breed(rab);
 			for(Rabbit rabbit: rab){
@@ -30,11 +30,15 @@ public class BreedingTime {
 	}
 	
 	private static ArrayList<Rabbit> breed(ArrayList<Rabbit> rab){
+	/*
+    method which takes an array list of super class Rabbit, and returns a copy of it, having removed the dead rabbits, and added newly born rabbits.
+	*/
 		ArrayList<Rabbit> newHutch = new ArrayList<Rabbit>(rab);
 		int iter = 0;
 		for(Rabbit rabbit : rab){
 			if (rabbit.isBreed() ){
-				newHutch.add(new RandomlyDyingPredetRabbit(5));	// RandomDyingRabbit()  // new DyingstaticRabbit(rabbit.diesAt())  //new Rabbits
+				newHutch.add(new RandomlyDyingPredetRabbit(5));	// RandomDyingRabbit()  // new DyingstaticRabbit(rabbit.diesAt())  //new Rabbits  
+				// I will learn how to change the type of Rabbit created without writing here in a few weeks
 				numOfRabbitsBorn++;
 			}
 			if (rabbit.isDead()){
